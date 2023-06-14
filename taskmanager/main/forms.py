004fe318764d7d3effc9e07fe41943c6c78712dd
Vasily <v1331v@mail.ru>
2023-06-14 16:35:05 +0300

    Интернет страница
    Переход между страницами и создание тасков

---
+++ taskmanager/main/forms.py
@@ -0,0 +1,18 @@
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "task"]
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'from-control',
+                'placeholder': 'Введите название'
+        }),
+            "task": Textarea(attrs={
+                'class': 'from-control',
+                'placeholder': 'Введите описание'
+            }),
+        }
